214558a3865118a21eaebaa062bc56d51dd21fca
Nikolas Lenning <nikolaslenning@gmail.com>
2020-09-15 18:37:50 -0600

    cards

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var card = document.querySelector('.card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
 });
+
+var card1 = document.querySelector('.card1');
+card1.addEventListener( 'click', function() {
+  card1.classList.toggle('is-flipped');
+});
